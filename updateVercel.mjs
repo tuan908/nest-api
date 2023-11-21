61032962139d643c6f64f605673dce4bc972abad
Nguyen Anh Tuan <tuanna@ait.com>
2023-11-21 09:53:15 +0700

    remove unnecessary

--- updateVercel.mjs
+++ updateVercel.mjs
@@ -2,31 +2,30 @@ import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 import fs from 'fs/promises';
 
+// __filename
 const esFilename = fileURLToPath(import.meta.url);
+// __dirname
 const esDirname = path.dirname(esFilename);
 
-async function main() {
-  let files = await fs.readdir(path.join(esDirname, 'dist'), {
+const distPath = path.join(esDirname, 'dist');
+const vercelConfigPath = path.join(esDirname, 'vercel.json');
+
+async function readFiles(distPath) {
+  return await fs.readdir(distPath, {
     encoding: 'utf8',
   });
-  for (const file of files) {
-    let data = `{"version":2,"builds":[{"src":"dist/${file}","use":"@vercel/node"}],"routes":[{"src":"/(.*)","dest":"dist/${file}"}]}`;
+}
 
-    let renderData = `
-services:
-- type: web
-  name: nest-api
-  env: node
-  repo: https://github.com/tuan908/nest-api.git
-  buildCommand: pnpm install
-  startCommand: node dist/${file}
-  plan: free
-  autoDeploy: false 
-    `;
-    fs.writeFile(path.join(esDirname, 'vercel.json'), data, {encoding: 'utf8'});
-    fs.writeFile(path.join(esDirname, 'render.yaml'), renderData, {
-      encoding: 'utf8',
-    });
+async function writeFile(filename, targetFilePath) {
+  let data = `{"version":2,"builds":[{"src":"dist/${filename}","use":"@vercel/node"}],"routes":[{"src":"/(.*)","dest":"dist/${filename}"}]}`;
+  await fs.writeFile(targetFilePath, data, {
+    encoding: 'utf8',
+  });
+}
+
+async function main() {
+  for (const file of await readFiles(distPath)) {
+    await writeFile(file, vercelConfigPath);
   }
 }
 
