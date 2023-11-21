4e208502f30a7806e00fd2ca54ead0293ea7465e
Nguyen Anh Tuan <tuanna@ait.com>
2023-11-21 15:29:47 +0700

    deps: update to latest

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('node:path');
+const {EsbuildPlugin} = require('esbuild-loader');
 
 module.exports = function (options) {
   /** @type {import('webpack').Configuration}*/
@@ -17,6 +18,27 @@ module.exports = function (options) {
       splitChunks: {
         chunks: 'all',
       },
+      minimizer: [
+        new EsbuildPlugin({
+          target: 'es2020',
+          format: 'esm',
+          minify: true,
+          treeShaking: true,
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        // Use esbuild instead of tsc to transpile:
+        {
+          test: /\.[jt]sx?$/,
+          loader: 'esbuild-loader',
+          options: {
+            target: 'es2020',
+            tsconfig: './tsconfig.build.json'
+          },
+        },
+      ],
     },
   };
 
